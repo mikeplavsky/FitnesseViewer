544e85bdd1d140e8ee2962dbdac365ed1c6d708c
Mike Plavsky <Mike.Plavsky@quest.com>
2010-11-11 21:15:39 +0300

    minor change

--- background.js
+++ background.js
@@ -3,12 +3,12 @@ var onRequest = function (request, sender, sendResponse) {
     chrome.pageAction.show(sender.tab.id);
     sendResponse({});
     
-}
+};
 
 chrome.extension.onRequest.addListener(onRequest);
 
 function handlePageIconClick(tab) {
     chrome.tabs.sendRequest(tab.id, { name: "toggle" }, function(){});
-}
+};
 
 chrome.pageAction.onClicked.addListener(handlePageIconClick);
