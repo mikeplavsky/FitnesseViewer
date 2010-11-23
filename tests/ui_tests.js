60ac34cd14060c2f81da374ffaa0b7a2890309e8
mikeplavsky <Mike.Plavsky@quest.com>
2010-11-23 21:55:04 +0300

    Beta: select where scenario is used

--- tests/ui_tests.js
+++ tests/ui_tests.js
@@ -21,7 +21,8 @@ module( 'ui', {
 			
 					[["Add Farms"],["Remove Backup"]],
 					[["Add File"]],
-					[["call PowerShell"],["Call Python"]]
+					[["call PowerShell"],["Call Python"]],
+                    [["Add File"]],
 					
 				]} 		
 			)
@@ -30,14 +31,14 @@ module( 'ui', {
         
         delete fitnesse.viewer.core.all_scenarios.res;
         delete core.parse_calls.res;
+        
+        toggle_scenarios();	
 	
 	} 
 
 });
 
 test( 'when clicking adress bar icon scenarios ui is either shown or hidden', function () {
-	
-	toggle_scenarios();	
 	
 	equals( $( '#fv-header' ).length, 1, 'header of Scenario Panel');
 	equals( $( '#fv-header' ).find( '.fv-view' ).length, 3, 'views of Scenario Panel' );
@@ -50,7 +51,6 @@ test( 'when clicking adress bar icon scenarios ui is either shown or hidden', fu
 
 test( 'switching between views', function () {
 	
-	toggle_scenarios();
 	ok( $( '#fv-view-all' ).text().match( /4/ ), 'number of discovered scenarios' );
 	
 	$( '#fv-view-all' ).click();
@@ -71,8 +71,6 @@ test( 'switching between views', function () {
 
 test( 'scenario selection', function () {
 
-	toggle_scenarios();
-
 	$( '#fv_view_all' ).click();
 
 	$( '.fv-scenario:eq(0)' ).click();
@@ -80,6 +78,20 @@ test( 'scenario selection', function () {
 	ok( $( '.fv-scenario:eq(0)' ).hasClass( 'fv-selected-scenario' ), 'sceanario is selected on click'  );	
 	same( $( '.fv-selected-scenario' ).length, 1, 'selected just one scenario'  );
 	
+});
+
+
+test ( 'showing scenario', function () {
+
+    $( '#fv_view_used' ).click();
+    $( '.fv-scenario:eq(1)' ).click();
+    
+    same( $( '.fv-selected-table' ).length, 1, 'just one scenario selected' );
+    same( $( '.fv-selected-row' ).length, 2, 'selected rows' );
+    
+    ok( $( 'td:contains(Add File)' ).closest( 'table' ).hasClass( 'fv-selected-table' ), 'selected scenario'  );
+    ok( $( 'td:contains(Add File)' ).closest( 'tr' ).hasClass( 'fv-selected-row' ), 'selected row'  );    
+    
 });
 
 
